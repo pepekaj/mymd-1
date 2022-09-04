c66d541e67b374794a5a104e58cb907df643753d
DaniBotz <108689250+YTDANIBOTZ@users.noreply.github.com>
2022-09-04 14:46:53 +0800

    Update _firstchat.js

--- plugins/_firstchat.js
+++ plugins/_firstchat.js
@@ -13,9 +13,9 @@ handler.all = async function (m) {
     let hao = `*Official Bot By @${'0'.split('@')[0]}* 
 *Powered By @${global.owner[1].split('@')[0]}*`
     if (new Date - user.pc < 86400000) return // setiap 24 jam sekali
-        await conn.send3ButtonLoc(m.chat, 'https://telegra.ph/file/606ecba4019ea293aa26f.jpg', ` ${ucapan()}, @${m.sender.split`@`[0]}
+        await conn.send3ButtonLoc(m.chat, 'https://telegra.ph/file/c4c3f666673ad816d4813.jpg', `${ucapan()}, @${m.sender.split`@`[0]}
  ${user.banned ? 'kamu dibanned' : `
-Saya Adalah Cute, Bot What'sapp Yang Bisa Anda Gunakan Untuk Membuat Sticker, Mendownload Sesuatu, Bermain Game, Dll.
+Saya Adalah Dani, Bot What'sapp Yang Bisa Anda Gunakan Untuk Membuat Sticker, Mendownload Sesuatu, Bermain Game, Dll.
 
 Tolong Jangan Spam, Telepon, Vc, Kirim Virtex.`} 
  `.trim(), hao, user.banned ? 'Pemilik Bot' : 'Menu', user.banned ? '!owner' : '.menu', 'Panduan penggunaan', '.tutorbot', 'Verify', '.daftar ${conn.getName(m.sender)}.16', m) 
